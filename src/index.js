9c28389e680dcc64f9962c057636af27f5019971
Yichuan Shen <shen.yichuan@gmail.com>
2020-03-12 23:40:39 +0100

    Add loadStrings() function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-module.exports = require('../index.json')
+let {readFile} = require('fs').promises
+let {resolve} = require('path')
+let index = require('../index.json')
+
+function safeModuleEval(input) {
+  return Function(`
+    "use strict"
+
+    let exports = {}
+    let module = {exports}
+
+    ;(() => {${input}})()
+
+    return module.exports
+  `)()
+}
+
+for (let locale in index) {
+  index[locale].loadStrings = async function() {
+    let content = await readFile(
+      resolve(__dirname, `./${locale}.i18n.js`),
+      'utf8'
+    )
+
+    return safeModuleEval(content)
+  }
+}
+
+module.exports = index
